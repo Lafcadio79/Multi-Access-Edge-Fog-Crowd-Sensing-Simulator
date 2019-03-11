be04a9a8314d269589dd339dba9b10dd9f59f015
Dimitri Belli <dimitribelli@hotmail.it>
2019-03-11 15:17:45 -0400

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo ************************************************
-echo * Multi-access Edge Fog Crowdsensing Simulator *
-echo *                                              *
-echo * @author:   Dimitri Belli                     *
-echo * @contact:  dimitri.belli@di.unipi.it         *
-echo * @license:  GPLv3                             *
-echo ************************************************
+echo "************************************************"
+echo "* Multi-access Edge Fog Crowdsensing Simulator *"
+echo "*                                              *"
+echo "* @author:   Dimitri Belli                     *"
+echo "* @contact:  dimitri.belli@di.unipi.it         *"
+echo "* @license:  GPLv3                             *"
+echo "************************************************"
 
 
-echo Initializing the simulator ...
+echo "Initializing the simulator . . ."
 
 python3 main.py
 
