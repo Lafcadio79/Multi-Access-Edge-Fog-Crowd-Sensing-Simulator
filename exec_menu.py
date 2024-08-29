0c62bda035c43643ac161a8fd2671942c443fbf3
Dimitri Belli <dimitri.belli@isti.cnr.it>
2024-08-29 09:38:31 +0200

    Update exec_menu.py

--- exec_menu.py
+++ exec_menu.py
@@ -3,9 +3,9 @@
 # Description: The module manages the user's communication interface through a  
 #              series of command line menus (execution)
 # 
-# Created on 24/feb/2019
+# Updated on Aug/29/2024
 # @authors: Dimitri Belli, Paolo Barsocchi, Antonino Crivello, Michele Girolami, 
-#           Davide La Rosa           
+#           Davide La Rosa          
 # License: GPLv3
 # Web: https://github.com/Lafcadio79/Multi-Access-Edge-Fog-Crowd-Sensing-Simulator
 ##################################################################################
