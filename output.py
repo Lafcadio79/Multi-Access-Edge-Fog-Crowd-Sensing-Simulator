fa562d8ba12fe3e7b64797bdea2037a80ee5df0e
Dimitri Belli <dimitri.belli@isti.cnr.it>
2025-02-06 14:06:13 +0100

    Update output.py

--- output.py
+++ output.py
@@ -3,7 +3,7 @@
 # Description: The module provides definitions to manage the simulator's input/ 
 #              output
 # 
-# Updated on Aug/29/2024
+# Updated on Feb/07/2025
 # @authors: Dimitri Belli, Paolo Barsocchi, Antonino Crivello, Michele Girolami, 
 #           Davide La Rosa  
 # License: GPLv3
